acdd8eb384fe014cafd4d976205ea3be184f5766
Skroter <digro@yandex.ru>
2024-07-01 14:23:52 +0500

    Добавьте файлы проекта.

---
+++ Topic4Lesson14Task1/Topic4Lesson14Task1.py
@@ -0,0 +1,10 @@
+﻿my_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+
+def rec(n):
+    if len(n) != 0:
+        print(my_list[0])
+        my_list.pop(0)
+        rec(n)
+    else: (print("Конец списка"))
+
+rec(my_list)
